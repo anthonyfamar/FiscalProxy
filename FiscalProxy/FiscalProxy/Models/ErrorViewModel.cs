4d7673e341e12c3e6c3fb69a2b406d001a4b2784
Anthony Famar <anthonysouza75@hotmail.com.br>
2026-02-11 14:28:01 -0300

    Criação do Projeto em MVC

---
+++ FiscalProxy/FiscalProxy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FiscalProxy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
